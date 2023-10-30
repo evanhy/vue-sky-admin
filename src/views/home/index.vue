d95d5300b1daf0d5aaafa966e8cc72027fe9c705
evansky <yhu8836@gmail.com>
2023-10-30 16:30:31 +0800

    feat: 新增husky和commitlint,移除simple-git-hooks

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,12 +6,20 @@ defineOptions({
 
 <template>
   <div>
-    <el-card>
-      <div class="h-100">
-        Home
-        <span class="i-carbon:user" />
-      </div>
-    </el-card>
+    <el-row :gutter="10" class="pb-10px">
+      <el-col v-for="index in 4" :key="index" :span="6">
+        <el-card class="min-h-40">
+          {{ index }}
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col v-for="index in 2" :key="index" :span="12">
+        <el-card class="min-h-120">
+          {{ index }}
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
